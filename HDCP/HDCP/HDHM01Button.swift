1fa540f0acfec9bd3146d56c3c4ddf64922f987c
AlbertXYZ <albert_xyz@163.com>
2016-01-05 18:39:29 +0800

    albert

---
+++ HDCP/HDCP/HDHM01Button.swift
@@ -0,0 +1,32 @@
+//
+//  HDHM01Button.swift
+//  HDCP
+//
+//  Created by 徐琰璋 on 16/1/5.
+//  Copyright © 2016年 batonsoft. All rights reserved.
+//
+
+import UIKit
+
+class HDHM01Button: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func titleRectForContentRect(contentRect: CGRect) -> CGRect {
+        
+        return CGRectMake(0, contentRect.size.height*0.7, contentRect.size.width, contentRect.size.height*0.2);
+    }
+    
+    override func imageRectForContentRect(contentRect: CGRect) -> CGRect {
+        
+        return CGRectMake(0, contentRect.size.height*0.1,contentRect.size.width,contentRect.size.height*0.6);
+    }
+
+    
+}

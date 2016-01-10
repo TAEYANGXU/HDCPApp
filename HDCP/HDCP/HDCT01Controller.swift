66cccbebfe86c6190a058b81f0eb9ff3a23c249a
AlbertXYZ <albert_xyz@163.com>
2016-01-10 17:04:50 +0800

    albert

--- HDCP/HDCP/HDCT01Controller.swift
+++ HDCP/HDCP/HDCT01Controller.swift
@@ -8,28 +8,195 @@
 
 import UIKit
 
-class HDCT01Controller: BaseViewController {
+class HDCT01Controller: UITableViewController {
 
+    let ct01Array = [[["title":"我的菜谱","image":"WDCPIcon"],
+        ["title":"我的收藏","image":"SCIcon"]],
+        [["title":"扫一扫","image":"SYSCIcon"],
+            ["title":"晒一晒","image":"SYSIcon"]],
+        [["title":"动态","image":"DTIcon"],
+            ["title":"设置","image":"SZIcon"]]]
+    
+    
+    var headerView:UIView?
+    var headerBg:UIImageView?
+    var headerIcon:UIImageView?
+    var loginBtn:UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - 创建UI视图
+    
+    func setupUI(){
+    
+        
+        headerView = UIView(frame: CGRectMake(0,0,Constants.kSCREENWITH,160))
+        headerView?.backgroundColor = UIColor.clearColor()
+        
+        /**
+        *   背景图
+        */
+        headerBg = UIImageView()
+        headerBg?.backgroundColor = UIColor.whiteColor()
+        headerBg?.image = UIImage(named: "userBGIcon")
+        headerView?.addSubview(headerBg!)
+        
+        headerBg?.snp_makeConstraints(closure: { (make) -> Void in
+            
+            make.width.equalTo(Constants.kSCREENWITH)
+            make.height.equalTo(160)
+            make.top.equalTo(headerView!).offset(0)
+            make.left.equalTo(headerView!).offset(0)
+            
+        })
+        
+        /**
+        *   头像
+        */
+        headerIcon = UIImageView()
+        headerIcon?.layer.cornerRadius = 40;
+        headerIcon?.layer.masksToBounds = true
+        headerIcon?.image = UIImage(named: "noDataDefaultIcon")
+        headerIcon?.backgroundColor = UIColor.redColor()
+        headerView?.addSubview(headerIcon!)
+        
+        headerIcon?.snp_makeConstraints(closure: { (make) -> Void in
+            
+            make.width.equalTo(80)
+            make.height.equalTo(80)
+            make.left.equalTo(headerView!).offset(Constants.kSCREENWITH/2-40)
+            make.top.equalTo(headerView!).offset(30)
+        })
+        
+        /**
+        *   登录或注册
+        */
+        loginBtn = UIButton()
+        loginBtn?.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        loginBtn?.setTitle("登录/注册", forState: UIControlState.Normal)
+        loginBtn?.titleLabel?.font = UIFont.systemFontOfSize(16)
+        loginBtn?.addTarget(self, action: "loginOrRegistAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        headerView?.addSubview(loginBtn!)
+        
+        loginBtn?.snp_makeConstraints(closure: { (make) -> Void in
+            
+            make.top.equalTo(headerIcon!.snp_bottom).offset(0)
+            make.width.equalTo(Constants.kSCREENWITH)
+            make.height.equalTo(40)
+            make.left.equalTo(headerView!).offset(0)
+            
+        })
+        
+        self.tableView.tableHeaderView = headerView;
+        
+        self.tableView?.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "mycell")
+        self.tableView.backgroundColor = Constants.HDBGViewColor
+        self.tableView.tableFooterView = UIView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - events
+    func loginOrRegistAction(btn:UIButton){
+    
+    
     }
-    */
-
+    
+    // MARK: - UITableView delegate/datasource
+    
+    override func tableView(tableView:UITableView, numberOfRowsInSection section: Int) ->Int
+    {
+        return ct01Array[section].count
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 3
+    }
+    
+    override func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) ->UITableViewCell
+    {
+        let cell = tableView .dequeueReusableCellWithIdentifier("mycell", forIndexPath: indexPath)
+        
+        /**
+         *  图标
+         */
+        var icon:UIImageView? = cell.viewWithTag(1000) as? UIImageView
+        if icon == nil {
+            
+            icon = UIImageView()
+            icon?.tag = 1000
+            cell.contentView.addSubview(icon!)
+            icon?.snp_makeConstraints(closure: { (make) -> Void in
+                
+                make.width.equalTo(20)
+                make.height.equalTo(20)
+                make.left.equalTo(cell.contentView).offset(16)
+                make.top.equalTo(cell.contentView).offset(12)
+                
+            })
+            
+        }
+        
+        
+        /**
+         *  名称
+         */
+        var title:UILabel? = cell.viewWithTag(2000) as? UILabel
+        if title == nil {
+            
+            title = UILabel()
+            title?.tag = 2000
+            title?.textColor = Constants.HDMainTextColor
+            title?.font = UIFont.systemFontOfSize(16)
+            cell.contentView.addSubview(title!)
+            title?.snp_makeConstraints(closure: { (make) -> Void in
+                
+                make.width.equalTo(200)
+                make.height.equalTo(44)
+                make.left.equalTo((icon?.snp_right)!).offset(16)
+                make.top.equalTo(cell.contentView).offset(0)
+            })
+        }
+        
+        /**
+         *  箭头
+         */
+        var arrow:UIImageView? = cell.viewWithTag(3000) as? UIImageView
+        if arrow == nil {
+            
+            arrow = UIImageView()
+            arrow?.tag = 3000
+            cell.contentView.addSubview(arrow!)
+            
+            arrow?.snp_makeConstraints(closure: { (make) -> Void in
+                
+                make.width.equalTo(20)
+                make.height.equalTo(20)
+                make.right.equalTo(cell.contentView).offset(-20)
+                make.top.equalTo(cell.contentView).offset(12)
+                
+            })
+            
+        }
+        
+        let array = ct01Array[indexPath.section]
+        icon?.image = UIImage(named: array[indexPath.row]["image"]!)
+        title?.text =   array[indexPath.row]["title"]
+        arrow?.image = UIImage(named: "arrowIcon")
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat(Constants.HDSpace)
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 44
+    }
+    
 }
+
--- HDCP/HDCP/MJRefresh/MJRefreshConst.h
+++ HDCP/HDCP/MJRefresh/MJRefreshConst.h
@@ -24,7 +24,7 @@
 #define MJRefreshColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
 
 // 文字颜色
-#define MJRefreshLabelTextColor MJRefreshColor(105, 149, 0)
+#define MJRefreshLabelTextColor MJRefreshColor(105, 105, 105)
 
 // 字体大小
 #define MJRefreshLabelFont [UIFont boldSystemFontOfSize:14]

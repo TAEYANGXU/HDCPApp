a7d56c65e90eab727f50c05ca6018f9d932afe48
AlbertXYZ <albert_xyz@163.com>
2016-02-18 16:46:33 +0800

    albert

--- HDCP/HDCP/HDCP-Bridging-Header.h
+++ HDCP/HDCP/HDCP-Bridging-Header.h
@@ -2,10 +2,12 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//指示视图
 #import "MBProgressHUD.h"
+//下拉刷新
 #import "MJRefresh.h"
 
-
+//社会分享
 #import <ShareSDK/ShareSDK.h>
 #import <ShareSDKConnector/ShareSDKConnector.h>
 //微信SDK头文件

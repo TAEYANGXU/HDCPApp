8c65c9047bfa66f60ce248f52285f7ef868f5745
AlbertXYZ <albert_xyz@163.com>
2016-03-16 10:18:55 +0800

    新功能、话题、豆友、动态

--- HDCP/HDCP/HDCT10Model.swift
+++ HDCP/HDCP/HDCT10Model.swift
@@ -40,19 +40,84 @@ class HDCT10Result: Mappable {
     
 }
 
-
-class HDCT10DataModel: Mappable {
+class HDCT10UserInfoModel: Mappable {
     
-    var ActionType:Int?
-    var Address:String?
-    var CommentCnt:Int?
-    var CommentCount:Int?
+    var avatar:String?
+    var gender:Int?
+    var intro:String?
+    var openUrl:String?
+    var relation:Int?
+    var userId:Int?
+    var userName:String?
+    var vip:Int?
+
     
     required init?(_ map: Map){
         mapping(map)
     }
     
     func mapping(map: Map) {
+        
+        avatar <- map["Avatar"]
+        gender <- map["Gender"]
+        intro <- map["Intro"]
+        openUrl <- map["OpenUrl"]
+        relation <- map["Relation"]
+        userId <- map["UserId"]
+        userName <- map["UserName"]
+        vip <- map["Vip"]
+    }
+    
+}
+
+
+class HDCT10DataModel: Mappable {
+    
+    var actionType:Int?
+    var address:String?
+    var commentCnt:Int?
+    var commentCount:Int?
+    var commentUrl:String?
+    var createTime:String?
+    var diggCnt:Int?
+    var diggCount:Int?
+    var diggId:Int?
+    var diggType:Int?
+    var entityType:Int?
+    var feedId:Int?
+    var formatTime:String?
+    var id:Int?
+    var images:Array<HDCT10ImagesModel>?
+    var intro:String?
+    var isDigg:Int?
+    var topicTags:Array<HDCT10TopicTagsModel>?
+    var url:String?
+    
+    required init?(_ map: Map){
+        mapping(map)
+    }
+    
+    func mapping(map: Map) {
+     
+        actionType <- map["ActionType"]
+        address <- map["Address"]
+        commentCnt <- map["CommentCnt"]
+        commentCount <- map["CommentCount"]
+        commentUrl <- map["CommentUrl"]
+        createTime <- map["CreateTime"]
+        diggCnt <- map["DiggCnt"]
+        diggCount <- map["DiggCount"]
+        diggId <- map["DiggId"]
+        diggType <- map["DiggType"]
+        entityType <- map["EntityType"]
+        feedId <- map["FeedId"]
+        formatTime <- map["FormatTime"]
+        id <- map["Id"]
+        images <- map["Images"]
+        intro <- map["Intro"]
+        isDigg <- map["IsDigg"]
+        topicTags <- map["TopicTags"]
+        url <- map["Url"]
         
     }
     

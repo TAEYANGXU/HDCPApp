7acb823ec1ffdd70ad4e1ea648c7cd3525783dac
徐琰璋 <albert_xyz@163.com>
2016-08-13 13:01:52 +0800

    cocoapods升级至1.0.1

--- HDCP/HDCP/AppDelegate.swift
+++ HDCP/HDCP/AppDelegate.swift
@@ -11,8 +11,6 @@ import CoreData
 import SDWebImage
 import ObjectMapper
 import Alamofire
-import ReachabilitySwift
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -149,39 +147,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func networkMonitoring(){
         
-        let reachability: Reachability
-        do {
-            reachability = try Reachability.reachabilityForInternetConnection()
-        } catch {
-            print("Unable to create Reachability")
-            return
-        }
-        
-        
-        reachability.whenReachable = { reachability in
-            // this is called on a background thread, but UI updates must
-            // be on the main thread, like this:
-            dispatch_async(dispatch_get_main_queue()) {
-                if reachability.isReachableViaWiFi() {
-                    print("Reachable via WiFi")
-                } else {
-                    print("Reachable via Cellular")
-                }
-            }
-        }
-        reachability.whenUnreachable = { reachability in
-            // this is called on a background thread, but UI updates must
-            // be on the main thread, like this:
-            dispatch_async(dispatch_get_main_queue()) {
-                print("Not reachable")
-            }
-        }
-        
-        do {
-            try reachability.startNotifier()
-        } catch {
-            print("Unable to start notifier")
-        }
+//        let reachability: Reachability
+//        do {
+//            reachability = try Reachability.reachabilityForInternetConnection()
+//        } catch {
+//            print("Unable to create Reachability")
+//            return
+//        }
+//        
+//        
+//        reachability.whenReachable = { reachability in
+//            // this is called on a background thread, but UI updates must
+//            // be on the main thread, like this:
+//            dispatch_async(dispatch_get_main_queue()) {
+//                if reachability.isReachableViaWiFi() {
+//                    print("Reachable via WiFi")
+//                } else {
+//                    print("Reachable via Cellular")
+//                }
+//            }
+//        }
+//        reachability.whenUnreachable = { reachability in
+//            // this is called on a background thread, but UI updates must
+//            // be on the main thread, like this:
+//            dispatch_async(dispatch_get_main_queue()) {
+//                print("Not reachable")
+//            }
+//        }
+//        
+//        do {
+//            try reachability.startNotifier()
+//        } catch {
+//            print("Unable to start notifier")
+//        }
     }
     
     /**
